6918e307b6d0845f14820d8df14cfee15e3a86e3
Vassil Kalkov <vassilkalkov@gmail.com>
2012-01-21 18:03:15 +0200

    Added more test cases to warehouse integration test

--- spec/requests/spree/admin/warehouses_spec.rb
+++ spec/requests/spree/admin/warehouses_spec.rb
@@ -5,8 +5,8 @@ describe "Warehouses" do
     sign_in_as!(Factory(:admin_user))
     #user = Factory(:admin_user, :email => "admin@person.com", :password => "password", :password_confirmation => "password")
     Spree::Ability.register_ability(AbilityDecorator)
-    
-    Factory(:warehouse, :name => 'huge_warehouse_1')
+
+    @warehouse1 = Factory(:warehouse, :name => 'huge_warehouse_1')
     Factory(:warehouse, :name => 'huge_warehouse_2')
 
     #visit spree.login_path
@@ -15,13 +15,38 @@ describe "Warehouses" do
     #click_button "Login"
     
     visit spree.admin_path
+    click_link "Warehouses"
   end
 
   context "listing warehouses" do
     it "should list existing warehouses" do
-
-      click_link "Warehouses"
-      #TODO make real check
+      page.should have_css('table#warehouses')
+      within("table#warehouses") do
+        page.should have_content("huge_warehouse_1")
+        page.should have_content("huge_warehouse_2")
+      end
     end
   end
+  
+  context "adding new warehouse" do 
+    before(:each) do 
+      click_link "New Warehouse"
+    end 
+
+    it "you should be redirected to warehouses/new view when you hit new warehouse button" do
+      page.should have_css('div#admin_warehouse_new_form')
+      fill_in "warehouse_name", :with => Faker::Name.name
+      click_button "Create"
+      page.should have_content("Warehouse was successfully created.")
+    end
+
+    it "should give an error if name is not unique" do
+      fill_in "warehouse_name" , :with => @warehouse1.name
+      click_button "Create"
+      page.should have_css('span.field_with_errors')
+      page.should have_content("has already been taken")
+      
+    end
+  end
+
 end
