860020efacb12defc263c1b04845a1c75785fb81
影月 零 <zero@genshin.org>
2012-01-24 17:51:48 +0900

    altered migration to avoid naming conflicts

--- db/migrate/20120124085003_create_spree_visual_code_types.rb
+++ db/migrate/20120124085003_create_spree_visual_code_types.rb
@@ -1,6 +1,6 @@
 class CreateVisualCodeTypes < ActiveRecord::Migration
   def change
-    create_table :visual_code_types do |t|
+    create_table :spree_visual_code_types do |t|
       t.string :name
       t.string :handler
 
