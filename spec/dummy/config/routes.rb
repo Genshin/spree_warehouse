26f1a93538d7c39d79f29005c4bc6e6ce437e25c
影月 零 <zero@genshin.org>
2012-01-24 17:02:22 +0900

    removed spec/dummy from ignore and added spec/dummy... should this be generated locally somehow after cloning? Why was it being ignored?

---
+++ spec/dummy/config/routes.rb
@@ -0,0 +1,4 @@
+Rails.application.routes.draw do
+
+  mount SpreeWarehouse::Engine => "/spree_warehouse"
+end
--- spec/spec/factories/factories.rb
+++
@@ -1,9 +0,0 @@
-FactoryGirl.define do
-  sequence(:unique_name) { |n| "Name #{n}" }
-
-  factory :warehouse , :class => Spree::Warehouse do 
-    name { Factory.next(:unique_name) }
-    details 'Warehouse Details'
-  end
-end
-
