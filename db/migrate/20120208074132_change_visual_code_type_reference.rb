e66eecf0025c2901703e2de47153900c3ede0eab
影月 零 <zero@genshin.org>
2012-02-08 20:14:56 +0900

    changed migration, !db will need to be rebuilt if you already migrated!

--- db/migrate/20120208074132_change_visual_code_type_reference.rb
+++
@@ -1,6 +0,0 @@
-class ChangeVisualCodeTypeReference < ActiveRecord::Migration
-  def change
-    rename_column :spree_visual_codes, :type, :type_id
-    #change_column :spree_visual_codes, :type_id, :references
-  end
-end
