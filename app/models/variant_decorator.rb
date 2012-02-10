5062e4c39a4df45b8b8e50d0046589ed89118bbb
影月 零 <zero@genshin.org>
2012-02-10 20:09:32 +0900

    small fixes

--- app/models/variant_decorator.rb
+++ app/models/variant_decorator.rb
@@ -1,10 +1,10 @@
 Spree::Variant.class_eval do
   has_one :visual_code
 
-  def self.find_bar_code(code)
-    Spree::Variant.where(:visual_code_id => Spree::VisualCode.where({:code => code, :type_id => Spree::VisualCodeType.where(:name => "BarCode")}))
+  def self.find_by_barcode(code)
+    Spree::Variant.where(:visual_code_id => Spree::VisualCode.where({:code => code, :type_id => Spree::VisualCodeType.where(:name => "Barcode")}))
   end
 
-  def self.set_bar_code(code)
+  def self.set_barcode(code)
   end
 end
