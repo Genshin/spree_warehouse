e09484c8f998008a8896605bed8382eb2b215c5b
doktor J <doktorj@gmail.com>
2012-02-09 23:24:27 -0800

    getting closer to figuring out how to save the non unique visual code nested within the other variant form data

--- app/models/spree/visual_code.rb
+++ app/models/spree/visual_code.rb
@@ -1,6 +1,8 @@
 class Spree::VisualCode < ActiveRecord::Base
   validates :code, :presence => true
   validates :type_id, :presence => true
-
+  
+  has_many :variants
+  #has_and_belongs_to_many :variants
   has_one :visual_code_type
 end
