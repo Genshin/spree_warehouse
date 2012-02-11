bbdcd4e3a95d271b23cc10ac2631655946d2e757
doktor J <doktorj@gmail.com>
2012-02-10 22:22:24 -0800

    renaming/and added product override

--- app/overrides/add_visual_code_to_product_form.rb
+++ app/overrides/add_visual_code_to_product_form.rb
@@ -1,4 +1,4 @@
-Deface::Override.new(:virtual_path => "spree/admin/variants/_form",
+Deface::Override.new(:virtual_path => "spree/admin/products/_form",
                       :name => "add_spree_warehouse_visual_codes_to_variant_form",
-                      :insert_bottom => "[data-hook='on_hand']",
+                      :insert_top => "ul#shipping-specs",
                       :partial => "spree/admin/visual_codes/visual_code_field.erb")
