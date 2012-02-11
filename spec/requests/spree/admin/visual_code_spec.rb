8f9cd3372f7d8348e4d6571fea97d00666dd86e3
Vassil Kalkov <vassilkalkov@gmail.com>
2012-02-11 13:45:08 +0200

    [spec] visual code & visual code types still failing

--- spec/requests/spree/admin/visual_code_spec.rb
+++ spec/requests/spree/admin/visual_code_spec.rb
@@ -5,8 +5,8 @@ describe Spree::VisualCode do
     sign_in_as!(Factory(:admin_user))
 
     #need types to check against
-    @visual_code_type_qr = Spree::VisualCodeType.find_or_create_by_name({:name => "QR Code", :handler => "qrcode"})
-    @visual_code_type_bar = Spree::VisualCodeType.find_or_create_by_name({:name => "BarCode", :handler => "barcode"})
+    @visual_code_type_qr = Spree::VisualCodeType.find_or_create_by_name({:name => "QR Code"})
+    @visual_code_type_bar = Spree::VisualCodeType.find_or_create_by_name({:name => "BarCode"})
 
     @visual_code1 = Spree::VisualCode.create(:code => "99954321", :type => Spree::VisualCodeType.where(:name => "BarCode"))
     @visual_code2 = Spree::VisualCode.create(:code => "[TEST] QR Code Data 1234 test", :type => Spree::VisualCodeType.where({:name => "QR Code", :handler => "qrcode"}))
@@ -16,8 +16,7 @@ describe Spree::VisualCode do
     click_link "Visual Codes"
   end
 
-  #TODO turn this into admin/visual_codes/types ?
-  context "GET /admin/visual_codes" do
+  context "visual codes" do
     it "should list visual codes" do
       within("table#visual_codes") do
         page.should have_content("99954321")
--- spec/requests/spree/admin/visual_code_type_spec.rb
+++ spec/requests/spree/admin/visual_code_type_spec.rb
@@ -3,8 +3,6 @@ require 'spec_helper'
 describe Spree::VisualCodeType do
   before (:each) do
     sign_in_as!(Factory(:admin_user))
-    Spree::Ability.register_ability(AbilityDecorator)
-
     @visual_code_type_qr = Factory(:visual_code_type, :name => "QR Code")
     @visual_code_type_bar = Factory(:visual_code_type, :name => "BarCode")
     
@@ -12,11 +10,9 @@ describe Spree::VisualCodeType do
     click_link "Warehouses"
     click_link "Visual Codes"
     click_link "Visual Code Types"
-    #visit "admin/visual_code_types"
   end
 
-  #TODO turn this into admin/visual_codes/types ?
-  context "GET /admin/visual_codes/types" do
+  context "visual code types" do
     it "should list visual code types" do
       within("table#visual_code_types") do
         page.should have_content("QR Code")
