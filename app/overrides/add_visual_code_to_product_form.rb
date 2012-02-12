bb1b04c2b39c8a4b8a643047cdc8bac9863682c8
doktor J <doktorj@gmail.com>
2012-02-11 23:42:04 -0800

    more cleanup

--- app/overrides/add_visual_code_to_product_form.rb
+++ app/overrides/add_visual_code_to_product_form.rb
@@ -1,4 +1,4 @@
 Deface::Override.new(:virtual_path => "spree/admin/products/_form",
                       :name => "add_spree_warehouse_visual_codes_to_variant_form",
                       :insert_top => "ul#shipping-specs",
-                      :partial => "spree/admin/visual_codes/visual_code_field.erb")
+                      :partial => "spree/admin/products/barcode_field.erb")
