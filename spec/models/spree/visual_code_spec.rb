adba2bcfa4be6238a0131d2cf9ef720f21273168
Vassil Kalkov <vassilkalkov@gmail.com>
2012-02-12 20:33:16 +0200

    Tests are passing, still need cleaning

--- spec/models/spree/visual_code_spec.rb
+++ spec/models/spree/visual_code_spec.rb
@@ -2,5 +2,7 @@ require 'spec_helper'
 
 describe Spree::VisualCode do
   it { should validate_presence_of(:code) }
-  it { should validate_presence_of(:type) }
+  it { should belong_to(:visual_code_type) }
+  #it { Factory(:visual_code).should validate_uniqueness_of(:code) }
+
 end
