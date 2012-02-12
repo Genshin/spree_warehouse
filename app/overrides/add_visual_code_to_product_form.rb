0e055b61352283167de147f3fca2208ac219ba40
doktor J <doktorj@gmail.com>
2012-02-11 23:44:49 -0800

    typo

--- app/overrides/add_visual_code_to_product_form.rb
+++ app/overrides/add_visual_code_to_product_form.rb
@@ -1,4 +1,4 @@
 Deface::Override.new(:virtual_path => "spree/admin/products/_form",
                       :name => "add_spree_warehouse_visual_codes_to_variant_form",
                       :insert_top => "ul#shipping-specs",
-                      :partial => "spree/admin/products/barcode_field.erb")
+                      :partial => "spree/admin/products/bar_code_field.erb")
