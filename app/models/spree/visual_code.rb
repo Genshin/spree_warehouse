3f8f61104b9dfcaf3d2490d7616e25ac1fbe9856
影月 零 <zero@genshin.org>
2012-02-12 20:54:47 +0900

    fixed visual code related methods

--- app/models/spree/visual_code.rb
+++ app/models/spree/visual_code.rb
@@ -6,11 +6,11 @@ class Spree::VisualCode < ActiveRecord::Base
   belongs_to :visual_code_type
 
   def self.find_or_create_by_code(code, code_type)
-    res = Spree::VisualCode.where({:code => code, :type_id => Spree::VisualCodeType.where(:name => code_type)})
+    res = Spree::VisualCode.where({:code => code, :visual_code_type_id => Spree::VisualCodeType.where(:name => code_type)})
     if res == []
       res = Spree::VisualCode.new
       res.code = code
-      res.type_id = Spree::VisualCodeType.where(:name => code_type)
+      res.visual_code_type_id = Spree::VisualCodeType.where(:name => code_type)
       res.save
     end
 
