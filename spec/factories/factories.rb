a7d52bc5010bb108c697c43eb586338236a74fa5
Vassil Kalkov <vassilkalkov@gmail.com>
2012-03-05 13:11:28 +0200

    Trying to fix Factory.next issue under travis 1.9.2

--- spec/factories/factories.rb
+++ spec/factories/factories.rb
@@ -1,27 +1,28 @@
 FactoryGirl.define do
-  sequence(:unique_name) { |n| "Name #{n}" }
+  sequence(:name) { |n| "Name #{n}" }
+  sequence(:unique_vc_type) { |n| "Name #{n}" }
   sequence(:unique_taxonomy_name) { |n| "Storage Hall #{n}" }
   sequence(:unique_container_taxon) { |n| "Shelve #{n}" }
  
   factory :warehouse , :class => Spree::Warehouse do 
-    name { Factory.next(:unique_name) }
+    name
   end
 
   factory :visual_code, :class => Spree::VisualCode do
-    code { Factory.next(:unique_name) }
-    visual_code_type Spree::VisualCodeType.where(:name => 'QR Code')
+    code { FactoryGirl.generate(:name) }
+    visual_code_type { Spree::VisualCodeType.where(:name => 'QR Code') }
   end
 
   factory :visual_code_type, :class => Spree::VisualCodeType do
-    name { Factory.next(:unique_name) }
+    name { FactoryGirl.generate(:unique_vc_type) }
   end
   
   factory :container_taxonomy, :class => Spree::ContainerTaxonomy do 
-    name { Factory.next(:unique_taxonomy_name)}
+    name { FactoryGirl.generate(:unique_taxonomy_name)}
   end
   
   factory :container_taxon, :class => Spree::ContainerTaxon do 
-    name { Factory.next(:unique_container_taxon)}
+    name { FactoryGirl.generate(:unique_container_taxon)}
   end
 
 end
