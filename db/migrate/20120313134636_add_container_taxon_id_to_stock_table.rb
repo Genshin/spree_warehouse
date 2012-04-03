d2d7c307b0281bdad713eec382433cf936d49044
Vassil Kalkov <vassilkalkov@gmail.com>
2012-04-03 11:07:14 +0300

    Delete stock migrations & create_spree_stock_records migration

--- db/migrate/20120313134636_add_container_taxon_id_to_stock_table.rb
+++
@@ -1,5 +0,0 @@
-class AddContainerTaxonIdToStockTable < ActiveRecord::Migration
-  def change
-    add_column :spree_stock, :container_taxon_id, :integer
-  end
-end
--- db/migrate/20120313140819_add_variant_id_to_stock_table.rb
+++
@@ -1,5 +0,0 @@
-class AddVariantIdToStockTable < ActiveRecord::Migration
-  def change
-    add_column :spree_stock, :variant_id, :integer
-  end
-end
