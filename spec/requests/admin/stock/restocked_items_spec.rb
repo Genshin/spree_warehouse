729ec59ce22c93b78e9c8918df85067b4040e74a
Vassil Kalkov <vassilkalkov@gmail.com>
2012-04-20 17:15:32 +0300

    [spec/requests/admin/stock/restocked_items_spec] Test sort and search by container taxon

--- spec/requests/admin/stock/restocked_items_spec.rb
+++ spec/requests/admin/stock/restocked_items_spec.rb
@@ -2,20 +2,26 @@ require 'spec_helper'
 
 describe "Restocked items" do
   before(:each) do
+    product1 = Factory(:product, :name => 'apache baseball cap', :available_on => '2011-01-01 01:01:01', :sku => "A100")
+    product2 = Factory(:product, :name => 'zomg shirt', :available_on => '2011-01-01 01:01:01', :sku => "Z100")
+
+    c_taxonomy = Factory(:container_taxonomy, :name => 'Rack#1') 
+    ct_shelve = Factory(:container_taxon, :container_taxonomy => c_taxonomy, :name => 'Shelve#1') 
+    ct_container = Factory(:container_taxon, :container_taxonomy => c_taxonomy, :name => 'Container#1', :parent_id => ct_shelve) 
+  
+    Factory(:stock_record, :variant => product1.master, :container_taxon => ct_shelve, :quantity => 5, :direction => 'in')
+    Factory(:stock_record, :variant => product2.master, :container_taxon => ct_container, :quantity => 10, :direction => 'in')
+
     sign_in_as!(Factory(:admin_user))
     visit spree.admin_path
+
+    click_link "Stock"
+    click_link "Restocked Items"
   end
 
   context "restocked items list" do
-    it "should list existing destocked items" do
-      product1 = Factory(:product, :name => 'apache baseball cap', :available_on => '2011-01-06 18:21:13:', :count_on_hand => '0')
-      product2 = Factory(:product, :name => 'zomg shirt', :available_on => '2125-01-06 18:21:13', :count_on_hand => '5')
-      Factory(:stock_record, :variant => product1.master, :quantity => 5, :direction => 'in')
-      Factory(:stock_record, :variant => product2.master, :quantity => 10, :direction => 'in')
+    it "should list and sort existing destocked items" do
 
-      click_link "Stock"
-      click_link "Restocked Items"
-      
       #restocked_before.asc
       within('table.index tr:nth-child(2)') { page.should have_content("zomg shirt") }
       within('table.index tr:nth-child(3)') { page.should have_content("apache baseball cap") }
@@ -24,7 +30,21 @@ describe "Restocked items" do
       click_link "admin_restocked_items_restocked_before"
       within('table.index tr:nth-child(2)') { page.should have_content("apache baseball cap") }
       within('table.index tr:nth-child(3)') { page.should have_content("zomg shirt") }
-      
+
+      #container_taxon.asc
+      click_link "admin_restocked_items_container_taxon"
+      within('table.index tr:nth-child(2)') { page.should have_content("zomg shirt") }
+      within('table.index tr:nth-child(2)') { page.should have_content("Container#1") }
+      within('table.index tr:nth-child(3)') { page.should have_content("apache baseball cap") }
+      within('table.index tr:nth-child(3)') { page.should have_content("Shelve#1") }
+
+      #container_taxon.desc
+      click_link "admin_restocked_items_container_taxon"
+      within('table.index tr:nth-child(2)') { page.should have_content("apache baseball cap") }
+      within('table.index tr:nth-child(2)') { page.should have_content("Shelve#1") }
+      within('table.index tr:nth-child(3)') { page.should have_content("zomg shirt") }
+      within('table.index tr:nth-child(3)') { page.should have_content("Container#1") }
+
       #variant_product_name.asc
       click_link "admin_restocked_items_product_name"
       within('table.index tr:nth-child(2)') { page.should have_content("apache baseball cap") }
@@ -61,39 +81,30 @@ describe "Restocked items" do
       within('table.index tr:nth-child(2)') { page.should have_content("zomg shirt") }
       within('table.index tr:nth-child(2)') { page.should have_content("10") }
       within('table.index tr:nth-child(3)') { page.should have_content("apache baseball cap") }
-      within('table.index tr:nth-child(3)') { page.should have_content("5") }
+      within('table.index tr:nth-child(3)') { page.should have_content("5") } 
 
-      
     end
   end
 
   context "searching restocked items" do
     it "should be able to search restocked items by product name, sku and container taxon " do
-      product1 = Factory(:product, :name => 'apache baseball cap', :available_on => '2011-01-01 01:01:01', :sku => "A100")
-      product2 = Factory(:product, :name => 'apache baseball cap2', :available_on => '2011-01-01 01:01:01', :sku => "B100")
-      product3 = Factory(:product, :name => 'zomg shirt', :available_on => '2011-01-01 01:01:01', :sku => "Z100")
-  
-      Factory(:stock_record, :variant => product1.master, :quantity => 5, :direction => 'in')
-      Factory(:stock_record, :variant => product2.master, :quantity => 5, :direction => 'in')
-      Factory(:stock_record, :variant => product3.master, :quantity => 5, :direction => 'in')
-
-      #Spree::Product.update_all :count_on_hand => 10
-
-      click_link "Stock"
-      click_link "Restocked Items"
 
       fill_in "search_variant_product_name_contains", :with => "ap"
       click_button "Search"
       #save_and_open_page
       page.should have_content("apache baseball cap")
-      page.should have_content("apache baseball cap2")
       page.should_not have_content("zomg shirt")
 
       fill_in "search_variant_sku_contains", :with => "A1"
       click_button "Search"
       page.should have_content("apache baseball cap")
-      page.should_not have_content("apache baseball cap2")
       page.should_not have_content("zomg shirt")
+
+      fill_in "search_container_taxon_name_contains", :with => "Shelve"
+      click_button "Search"
+      page.should have_content("apache baseball cap")
+      page.should_not have_content("zomg shirt")
+
     end
   end 
 end
