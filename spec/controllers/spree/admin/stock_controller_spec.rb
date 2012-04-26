078f1e1821656ec748ce6e501302153eb9085c06
Vassil Kalkov <vassilkalkov@gmail.com>
2012-04-26 05:28:59 +0300

    [spec/controllers/admin] stock_controller - test for flash msgs

--- spec/controllers/spree/admin/stock_controller_spec.rb
+++ spec/controllers/spree/admin/stock_controller_spec.rb
@@ -25,6 +25,7 @@ describe Spree::Admin::StockController do
       product.master.count_on_hand.should eql(0)
       post :restock, :stock_record => { :variant_id => product.master.id, :container_taxon_id => @ct_shelve.id, :quantity => 50, :direction => 'in'}, :use_route => :spree
       response.should redirect_to(spree.admin_stock_path)
+      flash[:notice].should_not be_nil
 
       product.reload
       product.master.count_on_hand.should eql(50)
@@ -45,7 +46,8 @@ describe Spree::Admin::StockController do
       product.master.count_on_hand.should eql(100)
 
       post :destock, :stock_record => { :variant_id => product.master.id, :container_taxon_id => @ct_shelve.id, :quantity => 100, :direction => 'out'}, :use_route => :spree
-
+      response.should redirect_to(spree.admin_stock_path)
+      flash[:notice].should_not be_nil
       product.reload
       product.master.variant_container_taxons.count.should eql(1)
       product.master.variant_container_taxons.active.count.should eql(0)
@@ -54,8 +56,4 @@ describe Spree::Admin::StockController do
     end
   end
 
-end
-
-
- 
-    
+end  
