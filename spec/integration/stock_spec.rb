7802801bb7d7dfe6222525f20f28d01c82bf9f12
Vassil Kalkov <vassilkalkov@gmail.com>
2012-04-26 05:31:27 +0300

    [spec/integration] Clean stock spec

--- spec/integration/stock_spec.rb
+++ spec/integration/stock_spec.rb
@@ -4,7 +4,7 @@ describe "change count_on_hand" do
   
   let(:product) { Factory(:product, :name => 'Subaru Outback') }
   
-  context "stock management" do 
+  context "count_on_hand" do 
     before do 
       Spree::Config.set :track_inventory_levels => true 
     end
@@ -14,18 +14,6 @@ describe "change count_on_hand" do
       product.master.add_count 5
       product.master.count_on_hand.should eql(205)
       product.on_hand.should eql(205)
-    end
-    
-    it "should increase variant count_on_hand after restock" do 
-      product.master.count_on_hand = 100
-      stock = Factory(:stock_record , :quantity => 100,  :variant => product.master, :direction => 'in') 
-      product.master.count_on_hand.should eql(200)
-    end
-
-    it "should decrease variant count_on_hand after destock" do 
-      product.master.count_on_hand = 100
-      stock = Factory(:stock_record , :quantity => 50,  :variant => product.master, :direction => 'out') 
-      product.master.count_on_hand.should eql(50)
     end
   end
 end
