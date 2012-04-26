d76c29e3359bbc4611d02923bdd59ab29cb0f984
Vassil Kalkov <vassilkalkov@gmail.com>
2012-04-26 10:48:28 +0300

    [spec/factories] Created variant_container_taxon factory

--- spec/models/variant_container_taxon_spec.rb
+++ spec/models/variant_container_taxon_spec.rb
@@ -5,6 +5,6 @@ describe Spree::VariantContainerTaxon do
   context "validations" do 
     it { should belong_to(:variant) }
     it { should belong_to(:container_taxon) }
+    it { should have_valid_factory(:variant_container_taxon) }
   end
-  
 end
