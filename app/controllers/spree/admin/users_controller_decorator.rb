e83dfc5b943177018d4517f461212348734d6cac
Vassil Kalkov <vassilkalkov@gmail.com>
2012-05-13 01:40:30 +0300

    Refactor QR generation

--- app/controllers/spree/admin/users_controller_decorator.rb
+++ app/controllers/spree/admin/users_controller_decorator.rb
@@ -3,16 +3,15 @@ require "rqrcode"
 module Spree
   module Admin
     UsersController.class_eval do 
-      def generate_qr_code
-        user = Spree::User.find(params[:id])
-        code = {profile: {name: user.email, server: request.host, port: request.port, key: user.api_key}}
+
+      def qr
+        user = User.find(params[:id])
+        code = { profile: { name: user.email, server: request.host, port: request.port, key: user.api_key } }
         @qr = RQRCode::QRCode.new(code.to_json, :size => 10, :level => :m)
 
-      	respond_to do |format|
-          format.html { render :partial => "spree/admin/users/generate_qr_code" }
-        end
-
+        respond_with { |format| format.html }
       end
+
   	end
   end
 end
