24bb6d1181a02c7c131955fd680aeac8c0e9ffac
Vassil Kalkov <vassilkalkov@gmail.com>
2012-05-31 19:45:00 +0300

    WIP : Add checkboxes for selectin container_taxonomies in warehouse edit view

--- app/controllers/spree/admin/warehouses_controller.rb
+++ app/controllers/spree/admin/warehouses_controller.rb
@@ -1,7 +1,25 @@
 module Spree
   module Admin
     class WarehousesController < ResourceController
-    
+
+      def edit
+        @container_taxonomies = ContainerTaxonomy.all
+        @warehouse = Warehouse.find(params[:id])
+      end
+
+      def update
+        @warehouse = Warehouse.find(params[:warehouse][:id])
+        respond_to do |format|
+          if @warehouse.update_attributes(params[:warehouse])
+            format.html { redirect_to @exam, notice: 'Exam was successfully updated.' }
+            format.json { head :no_content }
+          else
+            format.html { render action: "edit" }
+            format.json { render json: @warehouse.errors, status: :unprocessable_entity }
+          end
+        end
+      end
+  
     end
   end
 end
