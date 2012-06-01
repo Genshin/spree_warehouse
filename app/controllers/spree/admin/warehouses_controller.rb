292f2578f403a44d2523328f9288ab7acf852be3
Vassil Kalkov <vassilkalkov@gmail.com>
2012-06-01 18:37:44 +0300

    WIP : Warehouse delete into soft delete, ugly redirect after destroy action

--- app/controllers/spree/admin/warehouses_controller.rb
+++ app/controllers/spree/admin/warehouses_controller.rb
@@ -6,7 +6,26 @@ module Spree
         @container_taxonomies = ContainerTaxonomy.all
         @warehouse = Warehouse.find(params[:id])
       end
-      
+
+      def index
+      	@warehouses = Warehouse.active
+      end
+
+      def destroy
+        @warehouse = Warehouse.find(params[:id])
+        @warehouse.deleted_at = Time.now
+
+        respond_to do |format|
+          if @warehouse.save
+            format.html { redirect_to admin_warehouses_url, :notice => 'Warehouse was successfully deleted.' }
+            format.json { head :ok }
+          else
+            format.html { render :action => "edit" }
+            format.json { render :json => [:admin, @warehouse.errors], :status => :unprocessable_entity }
+          end
+        end
+      end
+
     end
   end
 end
