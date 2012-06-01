c75c116d1f873cb9f3d3919e867b40967b0937c2
Vassil Kalkov <vassilkalkov@gmail.com>
2012-06-01 19:01:09 +0300

    [spec/controllers/admin] Added a test for new warehouse

--- spec/controllers/spree/admin/warehouses_controller_spec.rb
+++ spec/controllers/spree/admin/warehouses_controller_spec.rb
@@ -13,12 +13,18 @@ describe Spree::Admin::WarehousesController do
     end
   end 
 
+
+   it "should create warehouse" do
+    spree_get :new
+    response.should render_template("admin/warehouses/new")
+   end
+
   context "destroying a warehouse" do
     let(:warehouse) do
       warehouse = create(:warehouse)
     end
 
-    it "marks at deleted the warehouse" do
+    it "marks deleted the warehouse" do
       spree_delete :destroy, :id => warehouse
       warehouse.reload.deleted_at.should_not be_nil
     end
