b5c5cc22cac44829193fab2e751f4e0b8a1cc79a
Vassil Kalkov <vassilkalkov@gmail.com>
2012-06-14 17:41:11 +0300

    WIP : Add address partial to form of Suppliers

--- app/models/spree/supplier.rb
+++ app/models/spree/supplier.rb
@@ -2,6 +2,8 @@ module Spree
   class Supplier < ActiveRecord::Base 
   	has_many :stock_records
 
-  	attr_accessible :name, :company
+  	attr_accessible :name, :company, :address1, :address2, :city, :zipcode, 
+  	                :phone_number, :mobile_number, :email, :website
+  	                
   end
 end
