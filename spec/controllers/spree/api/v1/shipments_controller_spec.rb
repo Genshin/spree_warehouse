a215740a14938587f029dd35fa63ea9b957b8de6
Vassil Kalkov <vassilkalkov@gmail.com>
2012-07-10 10:36:46 +0300

    [spec/controllers/api] Create inventory_units_controller_spec

--- spec/controllers/spree/api/v1/shipments_controller_spec.rb
+++ spec/controllers/spree/api/v1/shipments_controller_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 module Spree
   describe Spree::Api::V1::ShipmentsController do
     render_views
-  
+
     let(:order) { mock_model Spree::Order, :backordered? => false }
     let(:shipping_method) { mock_model Spree::ShippingMethod, :calculator => mock('calculator') }
     let(:shipment) do
@@ -23,6 +23,15 @@ module Spree
         puts json_response.to_json
         #json_response.should have_attributes(attributes)
         response.status.should == 200
+      end
+
+      it "should mark shipment as picked if all inventory units are picked" do
+        shipment.state.should == 'pending'
+        order.inventory_units.each do |iu|
+          iu.update_attribute_without_callbacks('state', 'sold')
+        end
+        shipment.state.should == 'picked'
+
       end
     end
 
